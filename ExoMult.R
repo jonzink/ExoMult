b3f2341a77437ef98ff9cd5c61587fcb69b0ba3b
Jon Zink <44911960+jonzink@users.noreply.github.com>
2019-01-17 20:35:00 -0800

    Update ExoMult.R

--- ExoMult.R
+++ ExoMult.R
@@ -4,7 +4,7 @@
 ### Devolped on R 3.4.3 ###
 
 ### If you make use of this code, please cite: ###
-### J. K. Zink, J. L. Christiansen, and  B. M. S. Hansen 2018, MNRAS
+### J. K. Zink, J. L. Christiansen, and  B. M. S. Hansen 2019, MNRAS 483, 4479 ###
 
 
 
